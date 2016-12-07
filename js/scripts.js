3302c9b25473e60dcf5d85b151577f242186611c
Joanna Strzelecka <joastr1@gmail.com>
2016-12-07 21:56:58 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+// jQuery
+
+$(function(){
+	var carouselList = $("#carousel ul");
+	var changeslide = setInterval(function() {
+		$ (carouselList).animate({marginLeft:-400},1000,function() {
+			$(this).find("li:last").after($(this).find("li:first"));
+			$(this).css({marginLeft:0});
+		})
+	},3000);
+});
+
